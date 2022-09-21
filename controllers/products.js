40cb9f2c4d24cbc235055f36db5cf28fda72bfd7
nathan sodja <sodjanathan@gmail.com>
2022-09-21 02:41:50 +0100

    feat: pagination

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getProductByField = async (req, res) => {
 	// depending on what property values are passed, we process them
-	const { name, featured, company, sort, fields } = req.query
+	const { name, featured, company, sort, fields,  } = req.query
 
 	const queryObject = {}
 	
@@ -37,6 +37,12 @@ const getProductByField = async (req, res) => {
 		const fieldsList = fields.split(',').join(' ')
 		result = result.select(fieldsList)
 	} 
+
+	const page = Number(req.query.page) || 1
+	const limit = Number(req.query.limit) || 10
+	const skip = (page - 1) * limit
+
+	result = result.skip(skip).limit(limit)
 
 	const products = await result
 	
