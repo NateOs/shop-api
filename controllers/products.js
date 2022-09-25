2b55f3629afe789a92dbf87210e31c9fc69b2c0d
nathan sodja <sodjanathan@gmail.com>
2022-09-25 21:26:45 +0100

    feat: number filtering, greater than, less than etc

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getProductByField = async (req, res) => {
 	// depending on what property values are passed, we process them
-	const { name, featured, company, sort, fields,  } = req.query
+	const { name, featured, company, sort, fields,numericFilters  } = req.query
 
 	const queryObject = {}
 	
@@ -37,13 +37,42 @@ const getProductByField = async (req, res) => {
 		const fieldsList = fields.split(',').join(' ')
 		result = result.select(fieldsList)
 	} 
-
+	// pagination
 	const page = Number(req.query.page) || 1
 	const limit = Number(req.query.limit) || 10
 	const skip = (page - 1) * limit
 
 	result = result.skip(skip).limit(limit)
 
+	// numeric filters  (greater than, less than or equal to)
+
+	if (numericFilters) {
+		const operatorMap = {
+			'>': '$gt',
+			'>=': '$gte',
+			'=':'$eq',
+			'<':'$lt',
+			'<=':'$lte',
+		}
+
+		const regEx = /\b(<|>|>=|=|<|<=)\b/g
+		let filters = numericFilters.replace(regEx,(match) => `-${operatorMap[match]}-`)
+	console.log(filters)
+		const options = [`price`, `rating`];
+		filters = filters.split(',').forEach(item => {
+			// array destructuring, values correspond to array indices
+			// of 0, 1, 2 of(price - $gt - 40, rating - $gt - 4)
+
+			const [field,operator,value] = item.split('-') 
+			// assign to queryObject variable the values corresponding
+			if (options.includes(field)) {
+				queryObject[field] = {[operator]:Number(value)};
+			}
+		})
+	 result = Products.find(queryObject)
+	}
+
+	
 	const products = await result
 	
 	res.status(200).json({nbHits: products.length, products })
